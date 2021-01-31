e5de7d781ad9212d3ef7f8c956fd4c2e9399bb79
Kyle Givler <xvegan88x@gmail.com>
2021-01-31 16:56:17 -0500

    Able to establish a connection to the IRC server

--- HappyIRCClientLibrary/IRCClient.cs
+++ HappyIRCClientLibrary/IRCClient.cs
@@ -1,4 +1,5 @@
 ﻿using HappyIRCClientLibrary.Config;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -13,14 +14,25 @@ namespace HappyIRCClientLibrary
         public int Port { get; private set; }
         public string NickName { get; private set; }
         public string RealName { get; private set; }
+        public bool Connected { get; private set; }
 
         private Thread listenThread;
         private readonly IConfig config;
+        private ILog log;
+        private TcpClient client;
 
-        public IRCClient (
+        /// <summary>
+        /// Create an IRC Client
+        /// </summary>
+        /// <param name="server">The IRC server to connect to</param>
+        /// <param name="port">The port to connect on</param>
+        /// <param name="nickname">The nickname to use</param>
+        /// <param name="realname">The Real name to use</param>
+        /// <param name="config">An instance of the Config class</param>
+        public IRCClient(
             string server,
-            int port, 
-            string nickname, 
+            int port,
+            string nickname,
             string realname,
             IConfig config)
         {
@@ -28,23 +40,45 @@ namespace HappyIRCClientLibrary
             Port = port;
             NickName = nickname;
             RealName = realname;
+
             this.config = config;
+            log = config.GetLogger("IRCClientLib");
         }
 
+        /// <summary>
+        /// Connect to the IRC Server
+        /// </summary>
         public void Connect()
         {
+            log.Debug($"Connecting to: {Server}:{Port}");
+
+            client = new TcpClient(Server, Port);
+
             listenThread = new Thread(new ThreadStart(ListenThread));
             listenThread.Start();
+
+            Thread.Sleep(1000);
+            SendMessageToServer($"NICK {NickName}\r\n");
+            SendMessageToServer($"USER {NickName} 0 * :{RealName}\r\n");
+
             //listenThread.Join();
         }
 
+        /// <summary>
+        /// Disconnect from the IRC server
+        /// </summary>
+        public void Disconnect()
+        {
+            client.Close();
+        }
+
+        /// <summary>
+        /// Listen for messages from the IRC sercer
+        /// </summary>
+        /// <param name="networkSteam">The network stream to listen on</param>
         private void ListenThread()
         {
-            var log = config.GetLogger("ClientLibListenThread");
-            log.Debug($"Connecting to: {Server}:{Port}");
-
-            var tcpClient = new TcpClient(Server, Port);
-            NetworkStream ns = tcpClient.GetStream();
+            NetworkStream ns = client.GetStream();
 
             while (true)
             {
@@ -52,10 +86,36 @@ namespace HappyIRCClientLibrary
                 int bytesRead = ns.Read(bytes, 0, bytes.Length);
 
                 var message = Encoding.ASCII.GetString(bytes, 0, bytesRead);
-                log.Debug(message);
-            }
+                log.Debug($"ListenThread: {message}");
 
-            tcpClient.Close();
+                if (message.ToUpperInvariant().StartsWith("PING"))
+                {
+                    RespondToPing(message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Respond to the server's ping
+        /// </summary>
+        /// <param name="ping">The ping message</param>
+        private void RespondToPing(string ping)
+        {
+            string response = $"PONG {ping.Substring(5)}\r\n";
+            SendMessageToServer(response);
+        }
+
+        /// <summary>
+        /// Send a message to the IRC server
+        /// </summary>
+        /// <param name="message"></param>
+        public void SendMessageToServer(string message)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] writeBuffer = Encoding.ASCII.GetBytes(message);
+
+            log.Debug($"Sending: {message}");
+            ns.Write(writeBuffer, 0, writeBuffer.Length);
         }
     }
 }
