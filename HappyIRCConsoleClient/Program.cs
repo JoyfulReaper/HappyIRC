e32ff86e03e10d73e9d1bd827aa23304db2fad9a
Kyle Givler <xvegan88x@gmail.com>
2021-01-31 12:21:07 -0500

    Add Initial Projects

---
+++ HappyIRCConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HappyIRCConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
