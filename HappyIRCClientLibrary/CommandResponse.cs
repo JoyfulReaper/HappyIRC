b7fb117ec0caae875f8072f0c889b93b2b19626e
Kyle Givler <xvegan88x@gmail.com>
2021-02-01 18:48:28 -0500

    Begin Enum with server response codes

---
+++ HappyIRCClientLibrary/CommandResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyIRCClientLibrary
+{
+    public enum CommandResponse
+    {
+        // 001 to 004 indicates the connection was successful
+        INVALID = 0,
+
+        RPL_WELCOME = 1, // "Welcome to the Internet Relay Network <nick>!<user>@<host>"
+        RPL_YOURHOST = 2, // "Your host is <servername>, running version <ver>"
+        RPL_CREATED = 3, // "This server was created <date>"
+        RPL_MYINFO = 4, // "<servername> <version> <available user modes> < available channel modes>"
+
+        RPL_BOUNCE = 5,
+        RPL_USERHOST = 302,
+        RPL_ISON = 303,
+        RPL_AWAY = 301,
+        RPL_UNAWAY = 305,
+        RPL_NOWAWAY = 305,
+        
+        ERR_NICKNAMEINUSE = 433 // "<nick> :Nickname is already in use"
+    };
+}
