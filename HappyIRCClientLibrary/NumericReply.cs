847794c9c30836210f72cf9ebea880999ee9b3d0
Kyle Givler <xvegan88x@gmail.com>
2021-02-01 21:00:14 -0500

    Expand known responses

--- HappyIRCClientLibrary/NumericReply.cs
+++ HappyIRCClientLibrary/NumericReply.cs
@@ -15,12 +15,23 @@ namespace HappyIRCClientLibrary
         RPL_MYINFO = 4, // "<servername> <version> <available user modes> < available channel modes>"
 
         RPL_BOUNCE = 5,
+        RPL_AWAY = 301,
         RPL_USERHOST = 302,
         RPL_ISON = 303,
-        RPL_AWAY = 301,
         RPL_UNAWAY = 305,
-        RPL_NOWAWAY = 305,
-        
-        ERR_NICKNAMEINUSE = 433 // "<nick> :Nickname is already in use"
+        RPL_NOWAWAY = 306,
+        RPL_TOPIC = 332, // "<channel> :<topic>"
+
+        ERR_NOSUCHCHANNEL = 403, // "<channel name> :No such channel"
+        ERR_TOOMANYCHANNELS = 405, // "<channel name> :You have joined too many channels"
+        ERR_TOOMANYTARGETS = 407, // "<target> :<error code> recipients. <abort message>"
+        ERR_NICKNAMEINUSE = 433, // "<nick> :Nickname is already in use"
+        ERR_UNAVAILRESOURCE = 437, // "<nick/channel> :Nick/channel is temporarily unavailable"
+        ERR_NEEDMOREPARAMS = 461, // "<command> :Not enough parameters"
+        ERR_BANNEDFROMCHAN = 474, // "<channel> :Cannot join channel (+b)"
+        ERR_INVITEONLYCHAN = 473, // "<channel> :Cannot join channel (+i)"
+        ERR_BADCHANNELKEY = 475,  // "<channel> :Cannot join channel (+k)"
+        ERR_CHANNELISFULL = 471, // "<channel> :Cannot join channel (+l)"
+        ERR_BADCHANMASK = 476,   // "<channel> :Bad Channel Mask"
     };
 }
