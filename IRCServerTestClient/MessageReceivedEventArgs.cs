8131f9effc9b82c9aa8a4d46662532f70976998a
Kyle Givler <xvegan88x@gmail.com>
2021-02-06 12:18:52 -0500

    Add event to fire when getting a message from the server

---
+++ IRCServerTestClient/MessageReceivedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using HappyIRCClientLibrary.Models;
+
+namespace IRCServerTestClient
+{
+    public class MessageReceivedEventArgs : System.EventArgs
+    {
+        public readonly ServerMessage ServerMessage;
+
+        public MessageReceivedEventArgs(ServerMessage serverMessage)
+        {
+            ServerMessage = serverMessage;
+        }
+    }
+}
