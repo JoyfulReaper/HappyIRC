31754af54a4394ad9ae4bcfc79a0a25be9285fd6
Kyle Givler <xvegan88x@gmail.com>
2021-02-08 20:21:45 -0500

    Show connecting message, stub for the simple IRC client

--- IRCServerTestClient/frmMain.cs
+++ IRCServerTestClient/frmMain.cs
@@ -1,4 +1,29 @@
-﻿using HappyIRCClientLibrary;
+﻿/*
+MIT License
+
+Copyright(c) 2021 Kyle Givler
+https://github.com/JoyfulReaper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using HappyIRCClientLibrary;
 using HappyIRCClientLibrary.Events;
 using HappyIRCClientLibrary.Models;
 using System;
@@ -8,18 +33,18 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IRCServerTestClient
 {
-    // this code blows
+    public enum Mode { Raw, Simple };
 
     public partial class frmMain : Form
     {
         public string Nick { get; set; } // setter prob shouldn't be public, but this is just test code!
         public string Real { get; set; } // setter prob shouldn't be public, but this is just test code!
+        public Mode Mode { get; set; }
 
         private IrcClient client;
 
@@ -31,14 +56,21 @@ namespace IRCServerTestClient
 
         private async Task Setup()
         {
-            var frm = new FrmUserName(this);
-            frm.ShowDialog(this);
+            var frmNick = new FrmUserName(this);
+            frmNick.ShowDialog(this);
 
             User user = new User(Nick, Real);
             Server server = new Server("irc.quakenet.org", 6667);
 
             client.Initialize(server, user);
-            await client.Connect(); //TODO Show something when waiting to connect...
+
+            frmConnecting frmCon = new frmConnecting();
+            frmCon.Show(this);
+
+            Application.DoEvents();
+
+            await client.Connect();
+            frmCon.Close();
 
             client.ServerMessageReceived += MessageReceived;
         }
@@ -48,11 +80,23 @@ namespace IRCServerTestClient
             await Setup();
         }
 
-
         private void btnSend_Click(object sender, EventArgs e)
         {
-            client.SendMessageToServer(txtSendToServer.Text + "\r\n");
+            if (Mode == Mode.Raw)
+            {
+                client.SendMessageToServer(txtSendToServer.Text + "\r\n");
+            }
+            else
+            {
+                ProccessCommand(txtSendToServer.Text);
+            }
+
             txtSendToServer.Text = string.Empty;
+        }
+
+        private void ProccessCommand(string command)
+        {
+            throw new NotImplementedException();
         }
 
         private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
