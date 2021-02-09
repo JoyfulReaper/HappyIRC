9a521e790981ba778e9b45b1d894a29857c80b42
Kyle Givler <xvegan88x@gmail.com>
2021-02-08 21:41:12 -0500

    Better compliance with the RFCs. Remove testing code.

--- HappyIRCClientLibrary/IRCClient.cs
+++ HappyIRCClientLibrary/IRCClient.cs
@@ -23,6 +23,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+// https://modern.ircdocs.horse/index.html
+
 using HappyIRCClientLibrary.Config;
 using log4net;
 using System.Collections.Generic;
@@ -44,7 +46,7 @@ namespace HappyIRCClientLibrary
         public User User { get; private set; } // The user to connect as
         public bool Connected { get; private set; } = false;// True if connected
         public bool Initialized { get; private set; } = false; // True if Initialized()
-        public List<Channel> Channels { get; private set; } = new List<Channel>(); // The Channels the cleint is in
+        public List<Channel> Channels { get; private set; } = new List<Channel>(); // The Channels the client is in
         public event EventHandler<ServerMessageReceivedEventArgs> ServerMessageReceived; // Envent that fire every time a message is received
 
         private readonly ILog log;
@@ -52,9 +54,8 @@ namespace HappyIRCClientLibrary
 
         private TcpConnection tcpConnection;
         private Task tcpConnectionTask;
-        private CancellationTokenSource cts = new CancellationTokenSource();
-        private readonly Queue<ServerMessage> messageQueue = new Queue<ServerMessage>();
-
+        private readonly CancellationTokenSource cts = new CancellationTokenSource();
+        //private readonly Queue<ServerMessage> messageQueue = new Queue<ServerMessage>();
 
         /// <summary>
         /// Create an IRC Client
@@ -83,6 +84,7 @@ namespace HappyIRCClientLibrary
 
         /// <summary>
         /// Connect to the IRC Server
+        /// See RFC 2812 Section 3.1: Connection Registration
         /// </summary>
         public async Task Connect()
         {
@@ -93,7 +95,14 @@ namespace HappyIRCClientLibrary
             // TODO look into this more
             Thread.Sleep(4000); 
 
+            if(!string.IsNullOrEmpty(Server.Password))
+            {
+                tcpConnection.SendMessageToServer($"PASS {Server.Password}\r\n");
+            }
+
             tcpConnection.SendMessageToServer($"NICK {User.NickName}\r\n");
+
+            //TODO Allow initial mode to be set: RFC2812 3.1.3 User message
             tcpConnection.SendMessageToServer($"USER {User.NickName} 0 * :{User.RealName}\r\n");
 
             while (!tcpConnection.Connected)
@@ -153,6 +162,18 @@ namespace HappyIRCClientLibrary
         {
             ThrowIfNotConnectedOrInitialized();
 
+            // Maximum message length per RFC 2812 is 512 characters
+            if(message.Length > 512)
+            {
+                log.Error("SendMessageToServer(): Message exceeds 512 characters");
+                throw new ArgumentOutOfRangeException(nameof(message), "message cannot exceed 512 characters");
+            }
+
+            if(!message.EndsWith("\r\n"))
+            {
+                log.Warn($"SendMessageToServer(): Message does not end in CR-LF: {message}");
+            }
+
             tcpConnection.SendMessageToServer(message);
         }
 
@@ -182,72 +203,72 @@ namespace HappyIRCClientLibrary
 
         ////////////////////////////// !!!NOTE: This stuff will be re-factored into a different class!!! ///////////////////////////
 
-        /// <summary>
-        /// Join a channel
-        /// </summary>
-        /// <param name="channel"></param>
-        public void Join(string channel)
-        {
-            //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
+        ///// <summary>
+        ///// Join a channel
+        ///// </summary>
+        ///// <param name="channel"></param>
+        //public void Join(string channel)
+        //{
+        //    //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
 
-            ThrowIfNotConnectedOrInitialized();
+        //    ThrowIfNotConnectedOrInitialized();
 
-            log.Info($"Attemping to join channel: {channel}");
-            SendMessageToServer($"JOIN {channel}\r\n");
-            Channel chan = new Channel() { Name = channel };
+        //    log.Info($"Attemping to join channel: {channel}");
+        //    SendMessageToServer($"JOIN {channel}\r\n");
+        //    Channel chan = new Channel() { Name = channel };
 
-            Channels.Add(chan);
+        //    Channels.Add(chan);
 
 
-            /*Possible replies:
-            ERR_NEEDMOREPARAMS ERR_BANNEDFROMCHAN
-           ERR_INVITEONLYCHAN ERR_BADCHANNELKEY
-           ERR_CHANNELISFULL ERR_BADCHANMASK
-           ERR_NOSUCHCHANNEL ERR_TOOMANYCHANNELS
-           ERR_TOOMANYTARGETS ERR_UNAVAILRESOURCE
-           RPL_TOPIC */
-        }
+        //    /*Possible replies:
+        //    ERR_NEEDMOREPARAMS ERR_BANNEDFROMCHAN
+        //   ERR_INVITEONLYCHAN ERR_BADCHANNELKEY
+        //   ERR_CHANNELISFULL ERR_BADCHANMASK
+        //   ERR_NOSUCHCHANNEL ERR_TOOMANYCHANNELS
+        //   ERR_TOOMANYTARGETS ERR_UNAVAILRESOURCE
+        //   RPL_TOPIC */
+        //}
 
-        /// <summary>
-        /// Part (leave) a channel
-        /// </summary>
-        /// <param name="channel">The channel to part</param>
-        public void Part(string channel)
-        {
-            //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
+        ///// <summary>
+        ///// Part (leave) a channel
+        ///// </summary>
+        ///// <param name="channel">The channel to part</param>
+        //public void Part(string channel)
+        //{
+        //    //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
 
-            ThrowIfNotConnectedOrInitialized();
+        //    ThrowIfNotConnectedOrInitialized();
 
-            log.Info($"Attemping to part channel: {channel}");
-            SendMessageToServer($"PART {channel}\r\n");
+        //    log.Info($"Attemping to part channel: {channel}");
+        //    SendMessageToServer($"PART {channel}\r\n");
 
-            Channels.Remove(Channels.Where(x => x.Name == channel).FirstOrDefault());
+        //    Channels.Remove(Channels.Where(x => x.Name == channel).FirstOrDefault());
 
-            /* Possible replies
-           ERR_NEEDMOREPARAMS              ERR_NOSUCHCHANNEL
-           ERR_NOTONCHANNEL */
-        }
+        //    /* Possible replies
+        //   ERR_NEEDMOREPARAMS              ERR_NOSUCHCHANNEL
+        //   ERR_NOTONCHANNEL */
+        //}
 
-        /// <summary>
-        /// Send a chat message
-        /// </summary>
-        /// <param name="target">The nick or channel to send the message to</param>
-        /// <param name="message">The message to send</param>
-        public void SendMessage(string target, string message)
-        {
-            //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
-            ThrowIfNotConnectedOrInitialized();
+        ///// <summary>
+        ///// Send a chat message
+        ///// </summary>
+        ///// <param name="target">The nick or channel to send the message to</param>
+        ///// <param name="message">The message to send</param>
+        //public void SendMessage(string target, string message)
+        //{
+        //    //TODO Error checking! Check to see if the client is in the channel first, check the server reply somewhow for the known responses
+        //    ThrowIfNotConnectedOrInitialized();
 
-            log.Debug($"Attempting to message: {target} Message: {message}");
-            SendMessageToServer($"PRIVMSG {target} :{message}\r\n");
+        //    log.Debug($"Attempting to message: {target} Message: {message}");
+        //    SendMessageToServer($"PRIVMSG {target} :{message}\r\n");
 
-            /*
-             Possible Replies
-           ERR_NORECIPIENT                 ERR_NOTEXTTOSEND
-           ERR_CANNOTSENDTOCHAN            ERR_NOTOPLEVEL
-           ERR_WILDTOPLEVEL                ERR_TOOMANYTARGETS
-           ERR_NOSUCHNICK
-           RPL_AWAY */
-        }
+        //    /*
+        //     Possible Replies
+        //   ERR_NORECIPIENT                 ERR_NOTEXTTOSEND
+        //   ERR_CANNOTSENDTOCHAN            ERR_NOTOPLEVEL
+        //   ERR_WILDTOPLEVEL                ERR_TOOMANYTARGETS
+        //   ERR_NOSUCHNICK
+        //   RPL_AWAY */
+        //}
     }
 }
