6cbd63800ebdfdae55b335fc4d6543fa7b013e4d
Kyle Givler <xvegan88x@gmail.com>
2021-02-08 23:47:01 -0500

    Remove unneeded factories
    I was super overthinking this

--- HappyIRCClientLibrary/Factories/IChannelFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using HappyIRCClientLibrary.Models;
-
-namespace HappyIRCClientLibrary.Factories
-{
-    public interface IChannelFactory
-    {
-        Channel Channel(string name, string key);
-    }
-}
